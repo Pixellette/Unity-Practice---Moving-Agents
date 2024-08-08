3c07da771d2771d7b08191269282a3d5a1e57a30
Brook P <brookpugmire@gmail.com>
2024-08-08 21:47:42 +1200

    Initial Commit. Flee & Seek behaviour

---
+++ Assets/Bot.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;  // find the cop
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flee(target.transform.position); // find the cop and set as target. 
+    }
+
+    void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        agent.SetDestination(this.transform.position - fleeVector);
+    }
+}
